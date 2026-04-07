bc3bc3085a83afaad62260abc9ec7abcfaa970c1
Navukkarasu-002 <navukkarasu2006@gmail.com>
2026-04-07 23:44:11 +0530

    Update Endtoend.java

--- com.div.project/src/test/java/com/div/project/tests/Endtoend.java
+++ com.div.project/src/test/java/com/div/project/tests/Endtoend.java
@@ -3,6 +3,7 @@ package com.div.project.tests;
 import com.div.project.base.BaseClass;
 import com.div.project.pages.*;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,33 +12,36 @@ public class Endtoend extends BaseClass {
     @Test
     public void fullFlow() {
 
-        // Login
+        
         LoginPage login = new LoginPage(driver);
         login.login("standard_user", "secret_sauce");
 
-        // Add Product
+        
         Productpage product = new Productpage(driver);
         product.addProductToCart();
         product.goToCart();
 
-        // Cart
+        
         CartPage cart = new CartPage(driver);
         cart.clickCheckout();
 
-        // Checkout
+        
         CheckoutPage checkout = new CheckoutPage(driver);
         checkout.completeCheckout("Raja", "Automation", "600001");
 
-        // Validation (VERY IMPORTANT)
+        
         String finalUrl = driver.getCurrentUrl();
         Assert.assertTrue(finalUrl.contains("checkout-complete"),
                 "Checkout failed!");
 
-        // Logout
+        
+        String successMessage = driver.findElement(By.className("complete-header")).getText();
+        Assert.assertEquals(successMessage, "Thank you for your order!");
+
+        
         LogoutPage logout = new LogoutPage(driver);
         logout.logout();
 
-        // Final log (correct place ✅)
         System.out.println("End test reached");
     }
 }
