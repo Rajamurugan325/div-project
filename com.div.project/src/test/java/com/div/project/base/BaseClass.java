ec60bcf175553021db62d896da0722a5adeb7424
Navukkarasu-002 <navukkarasu2006@gmail.com>
2026-04-07 23:14:35 +0530

    Update BaseClass.java

--- com.div.project/src/test/java/com/div/project/base/BaseClass.java
+++ com.div.project/src/test/java/com/div/project/base/BaseClass.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
 public class BaseClass {
-
+     // test commit
     public WebDriver driver;
 
     @BeforeMethod
