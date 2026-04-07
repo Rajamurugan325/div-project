e470486716822dc83533772ce18c685693425ee1
Navukkarasu-002 <navukkarasu2006@gmail.com>
2026-04-07 23:36:59 +0530

    Update Addtocart.java

--- com.div.project/src/test/java/com/div/project/tests/Addtocart.java
+++ com.div.project/src/test/java/com/div/project/tests/Addtocart.java
@@ -1,12 +1,24 @@
 package com.div.project.tests;
 
 import com.div.project.base.BaseClass;
+import com.div.project.pages.LoginPage;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Addtocart extends BaseClass {
 
     @Test
     public void addItemTest() {
-        System.out.println("Test Running...");
+
+        
+        LoginPage login = new LoginPage(driver);
+        login.login("standard_user", "secret_sauce");
+
+        
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+
+        
+        Assert.assertTrue(driver.getPageSource().contains("Remove"));
     }
 }
