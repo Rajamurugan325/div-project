bcf44603e2338d55a937d9d8fa0bc9f64521143d
Navukkarasu-002 <navukkarasu2006@gmail.com>
2026-04-07 23:46:19 +0530

    Update Logout.java

--- com.div.project/src/test/java/com/div/project/tests/Logout.java
+++ com.div.project/src/test/java/com/div/project/tests/Logout.java
@@ -3,6 +3,8 @@ package com.div.project.tests;
 import com.div.project.base.BaseClass;
 import com.div.project.pages.LoginPage;
 import com.div.project.pages.LogoutPage;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Logout extends BaseClass {
@@ -10,10 +12,16 @@ public class Logout extends BaseClass {
     @Test
     public void logoutTest() {
 
+        
         LoginPage login = new LoginPage(driver);
         login.login("standard_user", "secret_sauce");
 
+        
         LogoutPage logout = new LogoutPage(driver);
         logout.logout();
+
+        
+        boolean loginButtonVisible = driver.findElement(By.id("login-button")).isDisplayed();
+        Assert.assertTrue(loginButtonVisible, "Logout failed!");
     }
 }
